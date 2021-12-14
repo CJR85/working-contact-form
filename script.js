f8490f5360fbb11e62f501f6b6d9c5c337f367ca
Calvin Roberts <roberts.calvin85@gmail.com>
2021-12-14 23:38:49 +0000

    Enter subject and body credentials in sendEmail function

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function sendEmail() {
   Email.send({
     Host: 'smtp.gmail.com',
-    Username: 'username',
-    Password: 'password',
-    To: 'them@website.com',
-    From: 'you@isp.com',
-    Subject: 'This is the subject',
-    Body: 'And this is the body',
-  }).then((message) => alert(message));
+    Username: 'username', //Enter Email Here
+    Password: 'password', //Enter Password Here
+    To: 'them@website.com', //Enter Fwd Email Here
+    From: document.getElementById('email').value,
+    Subject: 'New Contact Form Enquiry  ',
+    Body:
+      'Name: ' +
+      document.getElementById('name').value +
+      '<br> Email: ' +
+      document.getElementById('email').value +
+      '<br> Phone Number: ' +
+      document.getElementById('phone').value +
+      '<br> Message: ' +
+      document.getElementById('message').value,
+  }).then((message) => alert('Message Sent Successfully'));
 }
